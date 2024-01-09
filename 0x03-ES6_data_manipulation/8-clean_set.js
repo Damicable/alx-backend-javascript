0227f9c4e33f1ae4a9970b1c6340bedbbee072c0
Damicable <annobidiegwu@gmail.com>
2024-01-09 22:26:30 +0000

    More mapping

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
-  const stringsArr = [];
-  set.forEach((word) => {
-    if (word.startsWith(startString)) {
-      stringsArr.push(word.substr(startString.length, word.length));
-    }
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
+
+  let endString = '';
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) endString += `${element.slice(startString.length)}-`;
   });
-  return stringsArr.join('-');
+
+  return endString.slice(0, endString.length - 1);
 }
