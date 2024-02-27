f65513cd3bf564e185626e60a8007374c0cb93f5
Damicable <annobidiegwu@gmail.com>
2024-02-27 20:38:14 +0000

    Complex HTTP

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,65 +1,37 @@
-// A small HTTP server using the http module
-
 const http = require('http');
 
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
 const port = 1245;
-const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1).map((line) => line.split(','));
-
-      const fields = {};
-      let totalStudents = 0;
-
-      for (const student of students) {
-        const [firstname, , , field] = student;
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-        totalStudents += 1;
-      }
-      let ret = '';
-      ret += 'This is the list of our students\n';
-      ret += `Number of students: ${totalStudents}\n`;
-      // eslint-disable-next-line guard-for-in
-      for (const field in fields) {
-        const count = fields[field].length;
-        const list = fields[field].join(', ');
-        ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
-      }
-
-      resolve(ret.slice(0, -1));
-    });
-  });
-}
-
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   const { url } = req;
 
   if (url === '/') {
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
-  if (url === '/students') {
-    const path = process.argv[2];
-    countStudents(path)
-      .then((output) => {
-        res.end(output);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+
+});
+
 module.exports = app;
