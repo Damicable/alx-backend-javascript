d17ab0e13c4f19b583a79b2e57701f2dd1b6379e
Damicable <annobidiegwu@gmail.com>
2024-02-27 20:47:53 +0000

    Corrected HTTP

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,75 +1,16 @@
-// A small HTTP server using Express module
-
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
-const port = 1245;
 const app = express();
-const path = process.argv[2];
-const fs = require('fs');
-
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1).map((line) => line.split(','));
-
-      const fields = {};
-      let totalStudents = 0;
-
-      for (const student of students) {
-        const [firstname, , , field] = student;
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-        totalStudents += 1;
-      }
-      let ret = '';
-      ret += 'This is the list of our students\n';
-      ret += `Number of students: ${totalStudents}\n`;
-      // eslint-disable-next-line guard-for-in
-      for (const field in fields) {
-        const count = fields[field].length;
-        const list = fields[field].join(', ');
-        ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
-      }
-
-      resolve(ret.slice(0, -1));
-    });
-  });
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/students', (req, res) => {
-  countStudents(path)
-    .then((output) => {
-      res.send(output);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
-
-app.use((req, res) => {
-  res.status(404).send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="utf-8">
-    <title>Error</title>
-    </head>
-    <body>
-    <pre>Cannot GET ${req.url}</pre>
-    </body>
-    </html>`);
-});
-
-app.listen(port);
+app.listen(1245);
 module.exports = app;
