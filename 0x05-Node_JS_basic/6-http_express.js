0131297e47cb7fc0d574660e11833e3206180495
Damicable <annobidiegwu@gmail.com>
2024-02-27 20:10:51 +0000

    node.js files

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,26 @@
+// A small HTTP server using Express module
+
+const express = require('express');
+
+const port = 1245;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.use((req, res) => {
+  res.status(404).send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Error</title>
+    </head>
+    <body>
+    <pre>Cannot GET ${req.url}</pre>
+    </body>
+    </html>`);
+});
+
+app.listen(port);
+module.exports = app;
