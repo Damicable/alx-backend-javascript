9adda8118e3c076bb5b229a766638c3c594bc3e5
Damicable <annobidiegwu@gmail.com>
2024-04-30 15:40:05 +0000

    Standardin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n);
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
@@ -7,4 +7,4 @@ process.stdin.on('readable', () => {
 });
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
-});process.stdout.write('Welcome to Holberton School, what is your name?\n');
+});
